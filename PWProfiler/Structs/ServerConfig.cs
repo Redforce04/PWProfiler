70043b0da067037ab38d895a0726b2bb189d3f3b
Redforce04 <redforcereturns@gmail.com>
2023-02-11 16:49:36 +0000

    Dev

---
+++ PWProfiler/Structs/ServerConfig.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="Log.cs" company="Redforce04#4091">
+// Copyright (c) Redforce04. All rights reserved.
+// </copyright>
+// -----------------------------------------
+//    Solution:         NetDataSL
+//    Project:          NetDataSL
+//    FileName:         ServerConfig.cs
+//    Author:           Redforce04#4091
+//    Revision Date:    02/05/2023 12:38 PM
+//    Created Date:     02/05/2023 12:38 PM
+// -----------------------------------------
+
+namespace PWProfiler.Structs;
+
+using System.Text.Json.Serialization;
+
+#pragma warning disable SA1313,SA1611
+
+/// <summary>
+/// The config for allowing an individual sl server to connect.
+/// </summary>
+public struct ServerConfig
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServerConfig"/> class.
+    /// </summary>
+
+    // ReSharper disable InconsistentNaming
+    public ServerConfig()
+    {
+    }
+    public ServerConfig(int Port, string ServerName, string Key)
+    {
+        this.Port = Port;
+        this.ServerName = ServerName;
+        this.Key = Key;
+    }
+
+    /// <summary>
+    /// Gets the port of the server.
+    /// </summary>
+    public int Port { get; private set; }
+
+    /// <summary>
+    /// Gets the name of the server.
+    /// </summary>
+    public string ServerName { get; private set; }
+
+    /// <summary>
+    /// Gets the key of the server.
+    /// </summary>
+    public string Key { get; private set; }
+}
